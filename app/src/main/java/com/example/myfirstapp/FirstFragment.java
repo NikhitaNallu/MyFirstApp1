6f5c27a062ed3ac43ceefe80310267f67c775e1a
nikkiN007 <80715958+nikkiN007@users.noreply.github.com>
2024-02-02 11:38:29 -0600

    completed app until task 9 -step 5

--- app/src/main/java/com/example/myfirstapp/FirstFragment.java
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,31 +12,54 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.myfirstapp.databinding.FragmentFirstBinding;
 
+import org.w3c.dom.Text;
+
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
+    TextView showCountTextView;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
+        //View fragmentFirsLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        //get the count text view
+        //showCountTextView = fragmentFirsLayout.findViewById(R.id.textview_first);
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View fragmentFirstLayout = binding.getRoot();
+        showCountTextView = binding.textviewFirst;
+
+        return fragmentFirstLayout;
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.countButton.setOnClickListener(new View.OnClickListener() {
+        /*binding.countButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
+        }
+        ); */
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countMe(view);
+            }
         });
+    }
+    private void countMe(View view){
+        //Get the value of the text view
+        String countString = showCountTextView.getText().toString();
+        //Convert value to a number and increment in
+        Integer count = Integer.parseInt(countString);
+        count++;
+        showCountTextView.setText(count.toString());
     }
 
     @Override
